1f1859f3c7057f096028074e43455a85f2be0b3d
Michelle <Michelle@DESKTOP-FF35JH5>
2019-10-01 18:25:48 +0200

    Subido

--- SpaceInvader/src/tp/p1/logic/objects/Bomb.java
+++ SpaceInvader/src/tp/p1/logic/objects/Bomb.java
@@ -1,5 +1,12 @@
 package tp.p1.logic.objects;
 
 public class Bomb {
-
+	private int fila;
+	private int columna;
+	private int harm = 1;
+	
+	private void move(int fila, int col) {
+		this.fila = fila;
+		this.columna = col;
+	}
 }
