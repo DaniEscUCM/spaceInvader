cf6575c0aff68d891f84cdd2add69b6836c31a2b
Michelle <Michelle@DESKTOP-FF35JH5>
2019-11-05 18:27:20 +0100

    nuevas clases e interfaces

---
+++ SpaceInvaderII/src/tp/p1/logic/lists/GameObjectBoard.java
@@ -0,0 +1,48 @@
+package tp.p1.logic.lists;
+
+import tp.p1.logic.objects.GameObject;
+
+public class GameObjectBoard {
+
+	private GameObject[] objects;
+	private int currentObjects;
+	public GameObjectBoard (int width, int height) {
+	// TODO implement
+	}
+	private int getCurrentObjects () {
+		return currentObjects;
+	// TODO implement
+	}
+	public void add (GameObject object) {
+	// TODO implement
+	}
+	private GameObject getObjectInPosition (int x, int y) {
+		return null;
+	// TODO implement
+	}
+	private int getIndex(int x, int y) {
+		return y;
+	// TODO implement
+	}
+	private void remove (GameObject object) {
+	// TODO implement
+	}
+	public void update() {
+	// TODO implement
+	}
+	private void checkAttacks(GameObject object) {
+	// TODO implement
+	}
+	public void computerAction() {
+	// TODO implement
+	}
+	private void removeDead() {
+	// TODO implement
+	}
+	public String toString(int x, int y) {
+		return null;
+	// TODO implement
+	}
+
+
+}
