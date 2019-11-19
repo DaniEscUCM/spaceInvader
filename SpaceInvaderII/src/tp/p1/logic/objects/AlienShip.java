a87f35c52d6e64efd57b070afdff9c1aeafd2d86
dani_ <dani_@LAPTOP-NA59IL22>
2019-11-19 19:38:58 +0100

    aliens

--- SpaceInvaderII/src/tp/p1/logic/objects/AlienShip.java
+++ SpaceInvaderII/src/tp/p1/logic/objects/AlienShip.java
@@ -5,7 +5,7 @@ import tp.p1.logic.Move;
 
 public class AlienShip extends EnemyShip{
 	
-	protected static int REMAINING_ALIENS = 0;
+	protected static int REMAINING_ALIENS = -2;
 	private static boolean IS_IN_FINAL_ROW;
 	private static int SHIPS_ON_BORDER;
 	protected int cyclesToMove;
@@ -16,6 +16,9 @@ public class AlienShip extends EnemyShip{
 		REMAINING_ALIENS ++;
 		SHIPS_ON_BORDER = 0;
 		this.dir=Move.LEFT;
+		if (game !=null) {
+			cyclesToMove=game.getLevel().getNumCyclesToMoveOneCell();
+		}
 		//cycleToMove se inicializa con el numero ciclos(segun level) en el que se mueve, en move decrece
 	}
 
@@ -24,8 +27,8 @@ public class AlienShip extends EnemyShip{
 	}
 
 	public static boolean haveLanded() {//hacen falta en main
-		// TODO Auto-generated method stub
-		return false;
+	
+		return IS_IN_FINAL_ROW;
 	}
 
 	public static boolean allDead() {//hacen falta en main
@@ -35,17 +38,21 @@ public class AlienShip extends EnemyShip{
 	
 	public void move() {//no sobre escribe si se quita ship
 		if(cyclesToMove==0) {
-			cyclesToMove=game.getLevel().getNumCyclesToMoveOneCell();
-			if(SHIPS_ON_BORDER>0 && !IS_IN_FINAL_ROW) {
+			cyclesToMove=game.getLevel().getNumCyclesToMoveOneCell();			
+			if(dir==Move.RIGHT) {this.y++;}
+			else if(dir==Move.LEFT) {this.y--;}	
+			this.border();						
+		}
+		else if(SHIPS_ON_BORDER>0) { //&& !IS_IN_FINAL_ROW
 				this.x++;
 				if(dir==Move.RIGHT) {dir=Move.LEFT;}
 				else if(dir==Move.LEFT) {dir=Move.RIGHT;}
 				SHIPS_ON_BORDER --;
-				}
-			if(dir==Move.RIGHT) {this.y++;}
-			else if(dir==Move.LEFT) {this.y--;}
-						
-		}
+				if(!IS_IN_FINAL_ROW && x==game.DIM_Y-1) {
+					IS_IN_FINAL_ROW=true;
+					}
+					
+			}
 		else cyclesToMove--;
 		/*
 		  	Si (cyclesToMove == 0)
@@ -59,6 +66,12 @@ public class AlienShip extends EnemyShip{
 				SHIPS_ON_BORDER -= 1
 			Sino cyclesToMove--
 		 */
+	}
+	
+	public void border() {
+		if((dir==Move.LEFT && y==0)||(dir==Move.RIGHT && y==game.DIM_X-1)) {
+			SHIPS_ON_BORDER=REMAINING_ALIENS;
+		}
 	}
 
 }
